6b922590bd8516ec5282c3e009923bc3704fb130
Robbie Harwood <rharwood@redhat.com>
2021-11-21 21:50:52 -0500

    Add note that we need escaping tests
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

---
+++ tests/escapes.c
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+/* such software.  many freedoms. */
+
+/* very TODO - tests for \u and stuff */
+int main() {
+    return 0;
+}
+
+/* Local variables: */
+/* c-basic-offset: 4 */
+/* indent-tabs-mode: nil */
+/* End: */
